0f574f34221766490ca5dc947dc860ec5d4860da
Ibrahim Benhmida <brahimbenhmida11@gmail.com>
2025-06-05 13:44:42 +0100

    uodate

--- button tests/button alone.cpp
+++ button tests/button alone.cpp
@@ -1,29 +1,27 @@
-#define BUTTON_PIN 34
+#define BUTTON_PIN 16  // GPIO16 supports internal pull-up
 
 bool buttonState = false;
-bool lastButtonRead = HIGH;
+bool lastButtonReading = HIGH;
 unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 50;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(BUTTON_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);  // Enable internal pull-up
+  Serial.println("TTGO Button Toggle Ready (GPIO16)");
 }
 
 void loop() {
   bool reading = digitalRead(BUTTON_PIN);
 
-  if (reading != lastButtonRead) {
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (lastButtonRead == HIGH && reading == LOW) {
+  
+    if (lastButtonReading == 1 && reading == 0) {
       buttonState = !buttonState;
-      Serial.print("Button state toggled: ");
-      Serial.println(buttonState ? 1 : 0);
+      Serial.print("Button toggled to: ");
+      Serial.println(buttonState);
     }
-  }
+  
+      Serial.println(buttonState);
 
-  lastButtonRead = reading;
+  lastButtonReading = reading;
 }
